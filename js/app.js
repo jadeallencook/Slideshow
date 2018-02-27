10864ee34ccc843200a8ada814a5ee0ffe33b717
Jade Allen Cook <jcook@deseretnews.com>
2018-02-27 09:08:17 -0700

    🛠 Initial build

--- js/app.js
+++ js/app.js
@@ -24,14 +24,14 @@
       page.setAttribute('data-page', x);
       if (x === app.page) page.style.opacity = '0.5';
       elem.appendChild(page);
-      page.onclick = function() {
+      page.onclick = function () {
         app.page = parseInt(this.getAttribute('data-page'));
         update();
       }
     }
   }();
 
-  var scene = function() {
+  var scene = function () {
     var info = data[app.page];
     document.querySelector('div.scene h1').innerText = info.header;
     document.querySelector('div.scene p span').innerText = info.body;
@@ -39,7 +39,7 @@
     if (info.opacity < 1) document.querySelector('div.scene div.background').style.opacity = info.opacity;
   }
 
-  var update = function() {
+  var update = function () {
     if (app.page >= data.length) app.page = 0;
     else if (app.page < 0) app.page = (data.length - 1);
     for (var x = 0, max = data.length; x < max; x++) {
--- js/data.js
+++ js/data.js
@@ -6,22 +6,25 @@ var data = [
     video: false,
     sound: false,
     opacity: 0.35,
-    position: 'center'
+    horizonal: 'center',
+    vertical: 'center'
   }, {
-    header: 'Introducing AMP Story!',
+    header: 'Introducing Slideshow',
     body: 'Cras mattis consectetur purus sit amet fermentum. Maecenas faucibus mollis interdum. Donec ullamcorper nulla non metus auctor fringilla. Nullam quis risus eget urna mollis ornare vel eu leo.',
     image: 'assets/world.jpg',
     video: false,
     sound: false,
     opacity: 0.35,
-    position: 'left'
+    horizonal: 'left',
+    vertical: 'center'
   }, {
-    header: 'Sollicitudin Sem Tristique Lorem',
+    header: 'Get Ready To Dive In',
     body: 'Nulla vitae elit libero, a pharetra augue. Etiam porta sem malesuada magna mollis euismod. Nulla vitae elit libero, a pharetra augue. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Maecenas sed diam eget risus varius blandit sit amet non magna.',
     image: 'assets/water.jpg',
     video: false,
     sound: false,
     opacity: 0.35,
-    position: 'right'
+    horizonal: 'right',
+    vertical: 'center'
   }
 ]
